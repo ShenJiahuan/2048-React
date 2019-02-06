ee4429ef06eb67676e7c3a23a07aa1c2d3afc6f0
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-02-06 12:19:15 +0800

    Created class NumberTable, extracted codes from Grid.js to App.js and NumberTable.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,108 @@
 import React, {Component} from 'react';
 import './App.css';
 import Grid from './Grid'
+import NumberTable from './NumberTable'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {grid: new Grid(), beginX: 0, beginY: 0, endX: 0, endY: 0, enableSwipe: false};
+    }
+
+    static getPosDuringEvent(e) {
+        var X, Y;
+        if (e.touches !== undefined) {
+            X = e.touches[0].screenX;
+            Y = e.touches[0].screenY;
+        } else {
+            X = e.screenX;
+            Y = e.screenY;
+        }
+        return [X, Y];
+    }
+
+    componentDidMount() {
+        var events = {
+            "keydown": this.onKeyPressed,
+            "touchstart": this.onTouchStart,
+            "mousedown": this.onTouchStart,
+            "touchmove": this.onTouchMove,
+            "touchend": this.onTouchEnd,
+            "mouseup": this.onTouchEnd,
+            "gesturestart": this.onGestureStart,
+        };
+
+        for (var e in events) {
+            document.addEventListener(e, events[e].bind(this));
+        }
+    }
+
+    onKeyPressed(e) {
+        var legalKey = false;
+        var grid = this.state.grid;
+        switch (e.keyCode) {
+            case 37:
+                legalKey = grid.action("left");
+                break;
+            case 38:
+                legalKey = grid.action("up");
+                break;
+            case 39:
+                legalKey = grid.action("right");
+                break;
+            case 40:
+                legalKey = grid.action("down");
+                break;
+            default:
+                break;
+        }
+        if (legalKey) {
+            grid.addValue();
+        }
+        this.setState({grid: grid});
+        e.preventDefault();
+    }
+
+    onTouchStart(e) {
+        var [X, Y] = App.getPosDuringEvent(e);
+        this.setState({beginX: X, beginY: Y, endX: X, endY: Y, enableSwipe: true});
+        e.preventDefault();
+    }
+
+    onTouchMove(e) {
+        var [X, Y] = App.getPosDuringEvent(e);
+        this.setState({endX: X, endY: Y});
+        this.setState({endX: X, endY: Y});
+        e.preventDefault();
+    }
+
+    onTouchEnd(e) {
+        var grid = this.state.grid;
+        var [beginX, beginY] = [this.state.beginX, this.state.beginY];
+        var [endX, endY] = [this.state.endX, this.state.endY];
+        if (Math.pow(endX - beginX, 2) + Math.pow(endY - beginY, 2) > 10 && this.state.enableSwipe) {
+            var legalKey = false;
+            if (Math.abs(beginX - endX) > Math.abs(beginY - endY)) {
+                legalKey = grid.action(beginX < endX ? "right" : "left");
+            } else {
+                legalKey = grid.action(beginY < endY ? "down" : "up");
+            }
+            if (legalKey) {
+                grid.addValue();
+            }
+        }
+        this.setState({grid: grid, enableSwipe: false});
+        e.preventDefault();
+    }
+
+    onGestureStart(e) {
+        this.setState({enableSwipe: false});
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <Grid />
+            <NumberTable numbers={this.state.grid.numbers}/>
         );
     }
 }
--- src/Grid.js
+++ src/Grid.js
@@ -1,6 +1,5 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
 import './App.css';
-import NumberRow from './NumberRow'
 
 class Grid extends Component {
     constructor(props) {
@@ -12,14 +11,14 @@ class Grid extends Component {
                 numbers[i][j] = "";
             }
         }
-        this.state = {numbers: numbers, beginX: 0, beginY: 0, endX: 0, endY: 0, enableSwipe: false};
+        this.numbers = numbers;
         for (var k = 0; k < 2; ++k) {
             this.addValue();
         }
     }
 
     upLeftTranspose() {
-        var original = this.state.numbers;
+        var original = this.numbers;
         var copy = Array(original[0].length);
         for (var k = 0; k < original[0].length; ++k) {
             copy[k] = Array(original.length);
@@ -29,16 +28,16 @@ class Grid extends Component {
                 copy[j][i] = original[i][j];
             }
         }
-        this.setState({numbers: copy});
+        this.numbers = copy;
     }
 
     upDownTranspose() {
-        var original = this.state.numbers;
+        var original = this.numbers;
         var copy = Array(original.length);
         for (var i = 0; i < original.length; ++i) {
             copy[i] = original[original.length - i - 1];
         }
-        this.setState({numbers: copy});
+        this.numbers = copy;
     }
 
     static getRandom() {
@@ -48,15 +47,13 @@ class Grid extends Component {
     }
 
     addValue() {
-        var numbers = this.state.numbers;
         while (true) {
             var [row, col] = Grid.getRandom();
-            if (numbers[row][col] === "") {
-                numbers[row][col] = Math.random() > 0.5 ? 4 : 2;
+            if (this.numbers[row][col] === "") {
+                this.numbers[row][col] = Math.random() > 0.5 ? 4 : 2;
                 break;
             }
         }
-        this.setState({numbers: numbers});
     }
 
     static move(numberRow) {
@@ -97,17 +94,15 @@ class Grid extends Component {
 
     leftMoveAndMerge() {
         var isChanged = false;
-        var numbers = this.state.numbers;
         for (var i = 0; i < 4; ++i) {
-            var action1 = Grid.move(numbers[i]);
-            numbers[i] = action1[0];
-            var action2 = Grid.merge(numbers[i]);
-            numbers[i] = action2[0];
-            var action3 = Grid.move(numbers[i]);
-            numbers[i] = action3[0];
+            var action1 = Grid.move(this.numbers[i]);
+            this.numbers[i] = action1[0];
+            var action2 = Grid.merge(this.numbers[i]);
+            this.numbers[i] = action2[0];
+            var action3 = Grid.move(this.numbers[i]);
+            this.numbers[i] = action3[0];
             isChanged = isChanged || action1[1] || action2[1] || action3[1];
         }
-        this.setState({numbers: numbers});
         return isChanged;
     }
 
@@ -135,102 +130,8 @@ class Grid extends Component {
         }
         return isChanged;
     }
-
-    static getPosDuringEvent(e) {
-        var X, Y;
-        if (e.touches !== undefined) {
-            X = e.touches[0].screenX;
-            Y = e.touches[0].screenY;
-        } else {
-            X = e.screenX;
-            Y = e.screenY;
-        }
-        return [X, Y];
-    }
-
-    componentDidMount() {
-        var events = {
-            "keydown": this.onKeyPressed,
-            "touchstart": this.onTouchStart,
-            "mousedown": this.onTouchStart,
-            "touchmove": this.onTouchMove,
-            "touchend": this.onTouchEnd,
-            "mouseup": this.onTouchEnd,
-            "gesturestart": this.onGestureStart,
-        };
-
-        for (var e in events) {
-            document.addEventListener(e, events[e].bind(this));
-        }
-    }
-
-    onKeyPressed(e) {
-        var legalKey = false;
-        switch (e.keyCode) {
-            case 37:
-                legalKey = this.action("left");
-                break;
-            case 38:
-                legalKey = this.action("up");
-                break;
-            case 39:
-                legalKey = this.action("right");
-                break;
-            case 40:
-                legalKey = this.action("down");
-                break;
-            default:
-                break;
-        }
-        if (legalKey) {
-            this.addValue();
-        }
-        e.preventDefault();
-    }
-
-    onTouchStart(e) {
-        var [X, Y] = Grid.getPosDuringEvent(e);
-        this.setState({beginX: X, beginY: Y, endX: X, endY: Y, enableSwipe: true});
-        e.preventDefault();
-    }
-
-    onTouchMove(e) {
-        var [X, Y] = Grid.getPosDuringEvent(e);
-        this.setState({endX: X, endY: Y});
-        e.preventDefault();
-    }
-
-    onTouchEnd(e) {
-        var [beginX, beginY] = [this.state.beginX, this.state.beginY];
-        var [endX, endY] = [this.state.endX, this.state.endY];
-        if (Math.pow(endX - beginX, 2) + Math.pow(endY - beginY, 2) > 10 && this.state.enableSwipe) {
-            var legalKey = false;
-            if (Math.abs(beginX - endX) > Math.abs(beginY - endY)) {
-                legalKey = this.action(beginX < endX ? "right" : "left");
-            } else {
-                legalKey = this.action(beginY < endY ? "down" : "up");
-            }
-            if (legalKey) {
-                this.addValue();
-            }
-        }
-        this.setState({enableSwipe: false});
-        e.preventDefault();
-    }
-
-    onGestureStart(e) {
-        this.setState({enableSwipe: false});
-        e.preventDefault();
-    }
-
     render() {
-        const numbers = this.state.numbers;
-        const numberTableContent = numbers.map((numberRow) =>
-            <NumberRow key={numberRow.id} numberRow={numberRow}/>
-        );
-        return (
-            <div className="number-table">{numberTableContent}</div>
-        );
+        return null;
     }
 }
 
