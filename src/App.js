fd3a7529575280b4b78ed2dc0898906893c5a6a9
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-02-06 10:16:05 +0800

    move() refactored to reduce complexity

--- src/App.js
+++ src/App.js
@@ -81,17 +81,19 @@ class Grid extends Component {
         var isMoved = false;
         var jInc = true;
         for (var j = 0; j < 3;) {
-            jInc = true;
-            if (numberRow[j] === "") {
-                for (var k = j; k < 3; ++k) {
-                    numberRow[k] = numberRow[k + 1];
-                    if (numberRow[k]) {
-                        isMoved = true;
-                        jInc = false;
-                    }
-                }
-                numberRow[3] = "";
+            if (numberRow[j] !== "") {
+                ++j;
+                continue;
             }
+            jInc = true;
+            for (var k = j; k < 3; ++k) {
+                numberRow[k] = numberRow[k + 1];
+                if (numberRow[k]) {
+                    isMoved = true;
+                    jInc = false;
+                }
+            }
+            numberRow[3] = "";
             if (jInc) {
                 ++j;
             }
