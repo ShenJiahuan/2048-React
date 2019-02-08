d3cbddba8543cdde405ed972f2f17375817cfb2d
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-02-08 12:44:31 +0800

    Changed font size and color

--- src/Score.js
+++ src/Score.js
@@ -5,8 +5,8 @@ class Score extends Component {
     render() {
         return (
             <div id="score-board">
-                <div id="score-info">分数</div>
-                <div id="score-value">{this.props.score}</div>
+                <div id="score-info"><div>SCORE</div></div>
+                <div id="score-value"><div>{this.props.score}</div></div>
             </div>
         );
     }
